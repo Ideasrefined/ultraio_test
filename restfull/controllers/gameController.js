efd840de52630f187dc46173644e0970bf0371a9
ali <ali@ideasrefined.com>
2019-05-26 02:46:38 +0500

    error handling OOP

--- restfull/controllers/gameController.js
+++ restfull/controllers/gameController.js
@@ -1,162 +1,235 @@
 var Game = require('../models/game.js');
 var Publisher = require('../models/publisher.js');
 
-exports.create = (req,res)=>
+var GameController = {
+
+
+create: (req,res,next)=>
 {
     let gamePublisher = req.body.publisher;
+    
+    try{
+
+    
     Publisher.findOne({name:gamePublisher},(err,pub)=>{
         if(err || !pub)
         {
-            res.status(404).send("Publisher " + gamePublisher + " not found");
-            return;
+            next(err);
+        }
+        else
+        {
+
+        
+            let gamePublisherId = pub._id;
+        
+            let gameDetail = {title:req.body.title, price:req.body.price,
+            publisher:gamePublisherId, tags:req.body.tags, release_date: req.body.release_date };
+
+    
+            var game = new Game(gameDetail);
+
+            game.save(function (err) {
+            if (err) {
+                console.log("ERROR inserting publisher:" + err);
+            return
+            }
+        
+            res.status(201).json({message:req.body.title + " Game created"});
+        });
+     }
+    })
+    }
+    catch(err)
+    {
+        next(err);
+    }
+
+    
+},
+
+update: (req,res,next)=>
+{
+
+    
+    let gamePublisher = req.body.publisher;
+    
+    try
+    {
+        Publisher.findOne({name:gamePublisher},(err,pub)=>{
+        if(err || !pub)
+        {
+            next(err)
         }
     
         let gamePublisherId = pub._id;
-    
-    let gameDetail = {title:req.body.title, price:req.body.price,
-        publisher:gamePublisherId, tags:req.body.tags, release_date: req.body.release_date };
+        
 
-   
-    var game = new Game(gameDetail);
-
-    game.save(function (err) {
-    if (err) {
-        console.log("ERROR inserting publisher:" + err);
-      return
-    }
-    
-    res.status(201).send(req.body.title + " Game created");
-    });
-})
-
-    
-}
-
-exports.update = (req,res)=>
-{
-
-    
-    let gamePublisher = req.body.publisher;
-    Publisher.findOne({name:gamePublisher},(err,pub)=>{
-        if(err || !pub)
-        {
-            res.status(404).send("Publisher " + gamePublisher + " not found");
-            return;
-        }
-    
-    let gamePublisherId = pub._id;
-    
-
-    let gameDetail = {title:req.body.title, price:req.body.price,
-        publisher:gamePublisherId, tags:req.body.tags, release_date: req.body.release_date };
+        let gameDetail = {title:req.body.title, price:req.body.price,
+            publisher:gamePublisherId, tags:req.body.tags, release_date: req.body.release_date };
 
 
-    Game.findOneAndUpdate({title:req.params.title}, 
-        gameDetail,
-        {omitUndefined:true}, 
-        (err,docs)=>{
-        if(err || !docs)
-            res.status(404).send("error updating document : " + err);
-        else
-            res.send("Update success");
+        Game.findOneAndUpdate({title:req.params.title}, 
+            gameDetail,
+            {omitUndefined:true}, 
+            (err,docs)=>{
+            if(err || !docs)
+                res.status(404).json({message:"error updating document : " + err});
+            else
+                res.json({message:"Update success"});
+            })
         })
-    })
-}
+    }
+    catch(err)
+    {
+        next(err);
+    }
 
-exports.index =  (req,res)=>
+},
+
+index: (req,res,next)=>
 {
+    try{
+
+    
     Game.find({},(err,docs)=>{
         if(err)
-                res.send("Error occured in Game listing:" + err);
+                next(err);
         else
                 res.send(docs); 
         });
-}
+    }
+    catch(err)
+    {
+        next(err);
+    }
+},
 
-exports.delete = (req,res)=>
+delete: (req,res,next)=>
 {
     let gamePublisher = req.params.publisher;
-    Publisher.findOne({name:gamePublisher},(err,pub)=>{
-        if(err || !pub)
-        {
-            res.status(404).send("Publisher " + gamePublisher + " not found");
-            return;
-        }
-    
-    let gamePublisherId = pub._id;
-    Game.findOneAndDelete({title:req.params.title,publisher:gamePublisherId},
-        (err,docs)=>{
+    try
+    {
 
-            if(err || !docs)
-            {       console.log("Error deleting object");
-                    res.status(404).send("Error deleting object" + err);
+    
+        Publisher.findOne({name:gamePublisher},(err,pub)=>{
+            if(err || !pub)
+            {
+                next(err);
             }
             else
             {
-                res.send(" deleted successfully");
+                let gamePublisherId = pub._id;
+        Game.findOneAndDelete({title:req.params.title,publisher:gamePublisherId},
+            (err,docs)=>{
+
+                if(err || !docs)
+                {       console.log("Error deleting object");
+                        res.status(404).json({message:"Error deleting object" + err});
+                }
+                else
+                {
+                    res.json({message:" deleted successfully"});
+                }
+            
+            
+            });
+
             }
         
         
+        })
+    }
+    catch(err)
+    {
+        next(err);
+    }
+},
+
+getOne: (req,res,next)=> {
+    try{
+
+    
+        Game.findOne({title:req.params.title})
+        .populate({path:'publisher',match:{name:req.params.publisher}}).exec((err,docs)=>{
+            if(err)
+                    next(err);
+                else if( !docs)
+                {
+                    res.status(404).json({message:"Game " + req.params.title + " not found"});
+                }
+                else
+                    res.send(docs);
         });
-    })
-}
-
-exports.getOne = (req,res) => {
-    Game.findOne({title:req.params.title})
-    .populate({path:'publisher',match:{name:req.params.publisher}}).exec((err,docs)=>{
-        if(err)
-                res.status(404).send("an error occured");
-            else if( !docs)
-            {
-                res.status(404).send("Game " + req.params.title + " not found");
-            }
-            else
-            res.send(docs);
-    });
-        
+    }
+    catch(err)
+    {
+        next(err);
+    }    
     
-}
+},
 
-exports.getPublisher = (req,res)=>{
+getPublisher: (req,res,next)=>{
+
+    try{
+
+    
     Game.findOne({title:req.params.title}).populate('publisher').exec((err,docs)=>{
         if(err)
-            res.status(404).send("An error occured");
+            next(err);
         else if(!docs)
         {
-            res.status(404).send("Game not found");
+            res.status(404).json({message:"Game not found"});
         }
         else
         {
             res.send(docs.publisher);
         }
     })
-}
+    }
+    catch(err)
+    {
+        next(err);
+    }
 
-exports.prune = (req,res)=>{
+},
+
+prune: (req,res,next)=>
+{
     let pruning_date = new Date();
     let twelve_month_date = new Date();
     pruning_date.setMonth(pruning_date.getMonth() - 18);
 
     twelve_month_date.setMonth(twelve_month_date.getMonth() - 12 );
 
+    try
+    {
+        
+    
     Game.deleteMany({release_date: { $lt:pruning_date }},(err,docs)=>{
         if(err)
         {
-            res.status(404).send("Error occured");
-            return;
+            next(err);
         }
     });
 
     Game.updateMany({release_date: {$gte:pruning_date, $lte:twelve_month_date}},
         { $mul: {price:0.80} }  , (err,docs)=>{
             if(err)
-            {    res.status(404).send("Error Occured in pruning:update : " + err);
-                return;
+            {    next(err);
+                
             }
             else
             {
-                res.send("Pruning complete");
+                res.json({message:"Pruning complete"});
             }
         } );
-
+    }
+    catch(err)
+    {
+        next(err);
+    }
 }
+
+};
+
+module.exports = GameController;
--- restfull/controllers/publisherController.js
+++ restfull/controllers/publisherController.js
@@ -1,62 +1,98 @@
 var Publisher = require('../models/publisher.js');
 
-exports.create = (req,res) =>
+var publisherController= {
+
+create: (req,res,next)=>
 {
     let publisherdetail = {name:req.body.name,siret:req.body.siret,phone:req.body.phone };
 
    
     var publisher = new Publisher(publisherdetail);
 
-    publisher.save( (err) =>{
-    if (err) {
-        res.status(404)("ERROR inserting publisher:" + err);
-      return
-    }
-    //console.log('New Publisher: ' + publisher);
-});
+        try
+        {
+                publisher.save( (err) =>{
+                        console.log(err);
+                if (err) {
+                        next(err);
+                }
+                else{
+                res.status(201).json({message:req.body.name + " publisher created"});
+                
+                }
+                });
+        }
+        catch(err)
+        {
+                next(err)
+        }
 
+        
+},
 
-        res.status(201).send(req.body.name + " Publisher created");
-}
-
-exports.update = (req,res) =>
+update: (req,res,next)=>
 {
+        try
+        {
+                Publisher.findOneAndUpdate({name:req.params.name}, 
+                {name:req.body.name,siret:req.body.siret,phone:req.body.phone},
+                {omitUndefined:true}, 
+                (err,docs)=>{
+                if(err || !docs)
+                        next(err);
+                else
+                        res.json({message:"Update success"});
+                })
+        }
+        catch(err)
+        {
+                next(err);
+        }
+},
 
-        Publisher.findOneAndUpdate({name:req.params.name}, 
-            {name:req.body.name,siret:req.body.siret,phone:req.body.phone},
-            {omitUndefined:true}, 
-            (err,docs)=>{
-            if(err || !docs)
-                res.status(404).send("error updating document");
-            else
-                res.send("Update success");
-        })
-}
-
-exports.index = (req,res)=>
+index: (req,res,next)=>
 {
+        try{
+
+        
         Publisher.find({},(err,docs)=>{
         if(err)
-                res.send("Error occured in publishers listing:" + err);
+                next(err);
         else
                 res.send(docs); 
         });
-
-}
-exports.delete = (req,res)=>
-{
-        Publisher.findOneAndDelete({name:req.params.name},(err,docs)=>{
-
-        if(err || !docs)
-        {       console.log("Error deleting object");
-                res.status(404).send("Error deleting object");
         }
-        else
+        catch(err)
         {
-            res.send(" deleted successfully");
+                next(err);
         }
+
+},
+
+delete: (req,res,next)=>
+{
+        try{
+
         
-        
-    });
+                Publisher.findOneAndDelete({name:req.params.name},(err,docs)=>{
+
+                if(err || !docs)
+                {     next(err);
+                }
+                else
+                {
+                        res.json({message:" deleted successfully"});
+                }
+                
+                
+        });
+        }
+        catch(err)
+        {
+                next(err);
+        }
 }
 
+};
+
+module.exports = publisherController;
